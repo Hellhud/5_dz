535af0a1d60093203b3aed9f6a1116e68e835e5a
Hellhound <hardoneloves@gmail.com>
2021-11-20 13:39:07 +0300

    Добавьте файлы проекта.

---
+++ 5_dz/5_dz.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main() {
+    int a;
+    std::cin >> a;
+    int temp = a, q = 0;
+    while (temp > 0) {
+        if (temp % 10 == 0)
+            q++;
+        temp /= 10;
+    }
+    while (q != 0) {
+        a = a / 10;
+        q--;
+    }
+    if (a < 99) {
+        if (a % 4 == 0) std::cout << "ves";
+        else std::cout << "neves";
+    }
+    else if (a < 999) {
+        if (a % 4 == 0) std::cout << "ves";
+        else std::cout << "neves";
+    }
+    else if (a <= 3000) {
+        if (a % 4 == 0) std::cout << "ves";
+        else std::cout << "neves";
+    }
+    return 0;
+}
